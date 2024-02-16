99a8f75a6a98674a8058bf1c9e5a9b57a38b9c72
himanshutl <himanshu.techlead@gmail.com>
2024-02-16 20:24:26 +0530

    creating order service for localhost

--- src/main/java/com/deliveryapp/orderservice/controller/OrderController.java
+++ src/main/java/com/deliveryapp/orderservice/controller/OrderController.java
@@ -2,26 +2,23 @@ package com.deliveryapp.orderservice.controller;
 
 import com.deliveryapp.orderservice.dto.OrderRequest;
 import com.deliveryapp.orderservice.dto.OrderResponse;
-import com.deliveryapp.orderservice.entity.Order;
 import com.deliveryapp.orderservice.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
+@CrossOrigin(origins = "*")
 public class OrderController {
 
     @Autowired
-    private OrderService orderService;;
+    private OrderService orderService;
 
     @PostMapping("/saveOrder")
     public ResponseEntity<OrderResponse> saveOrder(@RequestBody OrderRequest orderRequest) {
         OrderResponse orderSavedInDb =  orderService.saveOrderInDb(orderRequest);
-        return new ResponseEntity<OrderResponse>(orderSavedInDb, HttpStatus.CREATED);
+        return new ResponseEntity<>(orderSavedInDb, HttpStatus.CREATED);
     }
 }
